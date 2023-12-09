92103cdd1674d9b334d117d757107704c64b9e6c
stoneshik <strebilly@mail.ru>
2023-12-09 22:37:02 +0300

    переделал регистрацию

--- includes/scripts/main.js
+++
@@ -1,9 +0,0 @@
-// Вызов всех функций
-window.onload = function() {
-    const currentUrl = window.location.href;
-    const mainForm = document.getElementById('main_form');
-    const secondForm = document.getElementById('second_form');
-    if (mainForm !== undefined && secondForm !== undefined) {
-
-    }
-}
