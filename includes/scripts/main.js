059adcc900bb607e349972122c5e9a04e41e2150
stoneshik <strebilly@mail.ru>
2023-12-09 16:43:46 +0300

    готовый шаблон

---
+++ includes/scripts/main.js
@@ -0,0 +1,9 @@
+// Вызов всех функций
+window.onload = function() {
+    const currentUrl = window.location.href;
+    const mainForm = document.getElementById('main_form');
+    const secondForm = document.getElementById('second_form');
+    if (mainForm !== undefined && secondForm !== undefined) {
+
+    }
+}
